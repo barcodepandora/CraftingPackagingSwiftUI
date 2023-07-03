a0357a59e6f9239520414a745a75aa329e398753
Juan <jmoreno@novopayment.com>
2023-07-03 09:39:23 -0500

    POC 202307030939

--- CraftingPackagingSwiftUI/Package.swift
+++ CraftingPackagingSwiftUI/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CraftingPackagingSwiftUI",
+    platforms: [ .iOS(.v13) ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,6 +22,7 @@ let package = Package(
         .target(
             name: "CraftingPackagingSwiftUI",
             dependencies: []),
+            path: "CraftingPackagingSwiftUI/Sources/CraftingPackagingSwiftUI"),
         .testTarget(
             name: "CraftingPackagingSwiftUITests",
             dependencies: ["CraftingPackagingSwiftUI"]),
